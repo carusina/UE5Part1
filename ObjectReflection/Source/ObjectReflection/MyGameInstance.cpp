f1406b6c92807502d8207670b2235e53e5195187
unknown <msjjang0315@naver.com>
2025-09-17 19:18:51 +0900

    Unreal Reflection System

--- ObjectReflection/Source/ObjectReflection/MyGameInstance.cpp
+++ ObjectReflection/Source/ObjectReflection/MyGameInstance.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "MyGameInstance.h"
+#include "MyGameInstance.h" // 이 include가 첫번째 include
+#include "Student.h"
+#include "Teacher.h"
 
 UMyGameInstance::UMyGameInstance()
 {
@@ -13,6 +15,7 @@ void UMyGameInstance::Init()
 	Super::Init();
 
 	UE_LOG(LogTemp, Log, TEXT("==============================="));
+
 	UClass* ClassRunTime = GetClass();
 	UClass* ClassCompile = UMyGameInstance::StaticClass();
 
@@ -24,6 +27,34 @@ void UMyGameInstance::Init()
 	SchoolName = TEXT("Unreal 학교");
 	UE_LOG(LogTemp, Log, TEXT("학교 이름: %s"), *SchoolName);
 	UE_LOG(LogTemp, Log, TEXT("학교 이름 기본값: %s"), *GetClass()->GetDefaultObject<UMyGameInstance>()->SchoolName);
+
+	UE_LOG(LogTemp, Log, TEXT("==============================="));
+
+	UStudent* Student = NewObject<UStudent>();
+	UTeacher* Teacher = NewObject<UTeacher>();
+	Student->SetName(TEXT("학생1"));
+	UE_LOG(LogTemp, Log, TEXT("새로운 학생 이름: %s"), *Student->GetName());
+
+	FString CurrentTeacherName;
+	FString NewTeacherName(TEXT("이득우"));
+	FProperty* NameProp = UTeacher::StaticClass()->FindPropertyByName(TEXT("Name"));
+	if (NameProp)
+	{
+		NameProp->GetValue_InContainer(Teacher, &CurrentTeacherName);
+		UE_LOG(LogTemp, Log, TEXT("현재 선생님 이름: %s"), *CurrentTeacherName);
+
+		NameProp->SetValue_InContainer(Teacher, &NewTeacherName);
+		UE_LOG(LogTemp, Log, TEXT("새로운 선생님 이름: %s"), *Teacher->GetName());
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("==============================="));
+
+	Student->DoLesson();
+	UFunction* DoLessonFunc = Teacher->GetClass()->FindFunctionByName("DoLesson");
+	if (DoLessonFunc)
+	{
+		Teacher->ProcessEvent(DoLessonFunc, nullptr);
+	}
 
 	UE_LOG(LogTemp, Log, TEXT("==============================="));
 }
